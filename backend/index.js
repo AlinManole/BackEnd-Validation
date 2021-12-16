343961bbdd420ce1d6a0585af337ab29c7636bbc
Alin Manole <alin.manole@yahoo.com>
2021-12-16 10:53:25 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const express = require("express")
 const app = express()
 const port = 5000
 
-const users = require("./routes/users")
+const usersRoutes = require("./routes/users")
 
 app.use(express.json())
 
-app.use('/users', users)
+app.use('/users', usersRoutes)
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+    console.log(`Server is running on port ${port}`)
 })
